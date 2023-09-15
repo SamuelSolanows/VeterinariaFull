b6b70660255c0a801ed7375c11269bc1a438cdf5
Samuel <127533662+SamuelSolanows@users.noreply.github.com>
2023-09-14 23:45:27 -0500

    Interfaces

---
+++ app/src/main/java/com/example/veterinariafull/Adapter/VacunaAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.veterinariafull.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.veterinariafull.DB.Entitys.VacunaEntity
+import com.example.veterinariafull.R
+import com.example.veterinariafull.databinding.ItemVacunaBinding
+
+class VacunaAdapter(var Vacuna: MutableList<VacunaEntity>) :
+    RecyclerView.Adapter<VacunaAdapter.Holder>() {
+    inner class Holder(var binding: ItemVacunaBinding) : ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(
+            ItemVacunaBinding.bind(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_vacuna, parent, false)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        var vacuna = Vacuna.get(position)
+        vacuna.apply {
+            holder.binding.apply {
+                txtVacuna.text = Nombre
+                txtNumero.text = "$Id. "
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return Vacuna.size
+    }
+}
